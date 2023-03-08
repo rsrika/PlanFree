becb2f96ec9eeb14e7dde7300976c2609d272a05
Roshni Srikanth <rsrika@uw.edu>
2023-03-07 18:33:34 -0800

    adding pages

--- plan-free-app/src/js/About.js
+++ plan-free-app/src/js/About.js
@@ -6,9 +6,58 @@ export function About(props) {
     window.scrollTo(0, 0);
         
     return (
-    <div className='homePage split-color' >
+    <div className='Aboutpage' >
         <NavBar handleClickHome={props.handleClickHome} handleClickSurvey={props.handleClickSurvey} handleClickAbout={props.handleClickAbout} />
-        <div>This is the about page</div>
+        <div className='container'>
+            <div className="spacer"></div>
+            <div className='row'>
+                <div className='col'>
+                    <h1 className="about-title">Plan Free is <span className="blue">building bridges</span> and <span className="pink">making impact.</span></h1>
+                </div>
+                <div className='col'>
+                    <div className="half-spacer"/>
+                    <p className='descriptive-text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id enim vel nunc aliquet congue. Nulla sit amet laoreet lacus. More lorem ipsum text here. 
+                    </p>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='container'>
+                    <img className="mx-auto d-block" src="./img/Group 32.png" alt="drawing of two medical professionals with a bottle of pills in between"/>
+                </div>
+            </div>
+        </div>
+        <div className="hl"/>
+        <div className='spacer'/>
+        <div className='team-section'>
+            <div className='container'>
+                <p className="section-header">Meet the team</p>
+                <p className="descriptive-text">Get to know the passionate group of people making things happen at Plan Free.</p>
+            </div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='mx-auto col-4'>
+                        <img className="mx-auto d-block p-5" src="./img/Roshni.jpg" alt="Roshni Srikanth"/>
+                        <p className='descriptive-text'>Roshni Srikanth</p>
+                    </div>
+                    <div className='mx-auto col-4'>
+                        <img className="mx-auto d-block p-5" src="./img/Gargi.jpeg" alt="Gargi Panatula"/>
+                    </div>
+                    <div className='col-4'>
+                        <img className="mx-auto d-block p-5" src="./img/Miyu.jpeg" alt="Miyu Ozawa"/>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-4'>
+                        <img className="mx-auto d-block p-5" src="./img/James.jpeg" alt="James Zarowski"/>
+                    </div>
+                    <div className='col-4'>
+                        <img className="mx-auto d-block p-5" src="./img/Cindy.jpeg" alt="Cindy Chen"/>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     </div>
  );
 }
--- plan-free-app/src/js/Home.js
+++ plan-free-app/src/js/Home.js
@@ -8,7 +8,41 @@ export function Home(props) {
     return (
     <div className='homePage split-color' >
         <NavBar handleClickHome={props.handleClickHome} handleClickSurvey={props.handleClickSurvey} handleClickAbout={props.handleClickAbout} />
-        <div>This is the home page</div>
+        <div className='container'>
+            <div className="spacer"></div>
+            <div className='row'>
+                <div className='col col-text'>
+                    <div className='row title'>
+                        <span className="normal-title">KNOW MORE ABOUT</span>
+                        <br/>
+                        <span className="emergency">EMERGENCY</span>
+                        <br/>
+                        <span className="contraceptives">CONTRACEPTIVES</span>
+                    </div>
+                    <br/>
+                    <div className='row'>
+                        <p className="descriptive-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id enim vel nunc aliquet congue. Nulla sit amet laoreet lacus. Suspendisse malesuada arcu pretium, convallis elit ut, malesuada ex. Nunc quis pulvinar ante
+                        </p>
+                    </div>
+                </div>
+                <div className='col col-text'>
+                    <img className="image-home" src="./img/Group 31.png" alt="drawing of woman in lab coat leaning against a calendar" />
+                </div>
+            </div>
+        </div>
+        <div className="hl"/>
+        <br/>
+        <div className="map-section">
+            <div className='container'>
+                <p className="section-header">Immediate Resources</p>
+                <p className="descriptive-text">Where to go if you need immediate help on campus</p>
+            </div>
+            <div className='container'>
+                {/* MAP GOES HERE @GARGI */}
+            </div>
+            
+        </div>
     </div>
  );
 }
